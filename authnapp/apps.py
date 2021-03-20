f2a7b444da04eeb74cee8b185c9d1bd1406d1ead
vasiliy <vash050@rambler.ru>
2021-03-20 15:48:40 +0300

    -settings

--- authnapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthnappConfig(AppConfig):
-    name = "authnapp"
--- static/js/mytimer.js
+++
@@ -1,5 +0,0 @@
-function goLink() {
-  window.location.assign('/auth/login');
-}
-
-setTimeout(goLink, 10000);

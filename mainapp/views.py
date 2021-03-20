eb20770eb449011dcc44dc4ece04c26798fdbbfe
vasiliy <vash050@rambler.ru>
2021-03-20 16:18:35 +0300

    -debug

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,8 +5,6 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from basketapp.models import Basket
-
 from .models import Contact, Product, ProductCategory
 
 
@@ -17,14 +15,21 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
-def get_hot_product():
-    products = Product.objects.filter(is_active=True, category__is_active=True)
-    return random.sample(list(products), 1)[0]
+# def get_hot_product():
+#     products = Product.objects.filter(is_active=True, category__is_active=True)
+#     return random.sample(list(products), 1)[0]
 
 
-def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
-    return same_products
+# def get_same_products(hot_product):
+#     same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
+#     return same_products
+
+
+def get_hot_product_list():
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+    hot_product = random.sample(list(products), 1)[0]
+    hot_list = products.exclude(pk=hot_product.pk)[:3]
+    return (hot_product, hot_list)
 
 
 def products(request, pk=None, page=1):
@@ -32,7 +37,7 @@ def products(request, pk=None, page=1):
     links_menu = ProductCategory.objects.filter(is_active=True)
 
     if pk is not None:
-        if pk == '0':
+        if str(pk) == str(0):
             category = {"pk": 0, "name": "все"}
             products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
         else:
@@ -57,8 +62,9 @@ def products(request, pk=None, page=1):
             "media_url": settings.MEDIA_URL,
         }
         return render(request, "mainapp/products_list.html", content)
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
+    # hot_product = get_hot_product()
+    # same_products = get_same_products(hot_product)
+    hot_product, same_products = get_hot_product_list()
     content = {
         "title": title,
         "links_menu": links_menu,

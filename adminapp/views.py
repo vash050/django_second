c943ccb94c9ba9edebe390353a3bd6b0719ba976
vasiliy <vash050@rambler.ru>
2021-03-29 11:31:23 +0300

    -up

--- adminapp/views.py
+++ adminapp/views.py
@@ -92,20 +92,29 @@ class ProductCategoryCreateView(LoginRequiredMixin, CreateView):
     fields = "__all__"
 
 
+from django.db.models import F
+
+
 class ProductCategoryUpdateView(LoginRequiredMixin, UpdateView):
     model = ProductCategory
     template_name = "adminapp/category_update.html"
     success_url = reverse_lazy("admin:categories")
-    fields = "__all__"
+    form_class = ProductCategoryEditForm
 
     def get_context_data(self, **kwargs):
         context = super(ProductCategoryUpdateView, self).get_context_data(**kwargs)
         context["title"] = "категории/редактирование"
         return context
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs
+    def form_valid(self, form):
+        if "discount" in form.cleaned_data:
+            discount = form.cleaned_data["discount"]
+            if discount:
+                print(f"применяется скидка {discount}% к товарам категории {self.object.name}")
+                self.object.product_set.update(price=F("price") * (1 - discount / 100))
+                db_profile_by_type(self.__class__, "UPDATE", connection.queries)
+
+        return super().form_valid(form)
 
 
 class ProductCategoryDeleteView(LoginRequiredMixin, DeleteView):

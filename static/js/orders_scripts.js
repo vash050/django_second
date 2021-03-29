02411ca5b646245f49cf6aa60754b508a8b368d2
vasiliy <vash050@rambler.ru>
2021-03-29 11:08:22 +0300

    -queryset

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -19,7 +19,8 @@ $(document).ready(function () {
             var item_price = 0;
         } else {
             var item_price = parseFloat($price_object.text());
-        };
+        }
+        ;
         var quantity_delta = $(this).val() - $currentState.find("[name=" + $(this).attr("name") + "]").val();
         $order_total_quantity.text($quantity_total_current + quantity_delta);
         $order_total_cost.text(Number($cost_total_current + quantity_delta * item_price).toFixed(2));
@@ -48,7 +49,8 @@ $(document).ready(function () {
         $("input[type='number']").each(function () {
             if (!$(this).val()) {
                 $(this).val(0).prop('disabled', true);
-            };
+            }
+            ;
         });
         // Renew current state structure
         $currentState = $(".formset_row").clone();
@@ -62,7 +64,8 @@ $(document).ready(function () {
             var item_price = 0;
         } else {
             var item_price = parseFloat($price_object.text());
-        };
+        }
+        ;
         $order_total_cost.text(Number(parseFloat($order_total_cost.text()) - quantity_delta * item_price).toFixed(2));
         $currentState = $(".formset_row").clone();
     };
@@ -76,6 +79,6 @@ $(document).ready(function () {
         prefix: 'orderitems',
         added: setDefaultValue,
         removed: itemDelete,
-        hideLastAddForm: true
+        hideLastAddForm: false,
     });
 });
